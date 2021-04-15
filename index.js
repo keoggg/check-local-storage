517abce93fee0a14de152bc1f3fb1e794eca1e27
keoggg <82596235+keoggg@users.noreply.github.com>
2021-04-15 16:09:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ try {
   console.log('localStorage access successful');
 } catch (error) {
   console.log('access denied for localStorage');
-  window.open.postMessage('localStorage not accessible', '*');
+  window.open.postMessage('localStorage not accessible', 'https://zino.pro/app_dev.php/en/');
 }
