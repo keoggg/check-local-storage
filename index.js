8044c872c197a0b354aaddd971e95e65b49869af
keoggg <82596235+keoggg@users.noreply.github.com>
2021-04-16 14:03:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-(function () {
-  try {
-    window.localStorage.setItem('test', 'test');
-    console.log('localStorage access successful');
-  } catch (error) {
-    console.log('access denied for localStorage');
-    const iframe = document.querySelector('.check-incognito');
-    
-    if (iframe) {      
-      iframe.postMessage('localStorage not accessible', '*');
+document.addEventListener('DOMContentLoaded', () => {
+  (function () {
+    try {
+      window.localStorage.setItem('test', 'test');
+      console.log('localStorage access successful');
+    } catch (error) {
+      console.log('access denied for localStorage');
+      const iframe = document.querySelector('.check-incognito');
+      
+      console.log(iframe);
+
+      if (iframe) {
+        iframe.postMessage('localStorage not accessible', '*');
+      }
     }
-  }
-})();
+  })();
+});
