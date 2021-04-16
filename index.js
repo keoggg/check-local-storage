8db4c7996ce134bed90975d2719e9a48ddc8267c
keoggg <82596235+keoggg@users.noreply.github.com>
2021-04-16 10:58:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-try {
-  window.localStorage.setItem('test', 'test');
-  console.log('localStorage access successful');
-} catch (error) {
-  console.log('access denied for localStorage');
-  window.open.postMessage('localStorage not accessible', 'https://zino.pro/app_dev.php/en/');
-}
+(function () {
+  try {
+    window.localStorage.setItem('test', 'test');
+    console.log('localStorage access successful');
+  } catch (error) {
+    console.log('access denied for localStorage');
+    window.open.postMessage('localStorage not accessible', 'https://zino.pro/app_dev.php/en/');
+  }
+})();
