c66080e219bc34a93e5401587a0b3672d46bd7cb
keoggg <82596235+keoggg@users.noreply.github.com>
2021-04-16 15:35:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (function () {
-    try {
-      window.localStorage.setItem('test', 'test');
-      console.log('localStorage access successful');
-    } catch (error) {
-      console.log('access denied for localStorage');      
-      window.parent.postMessage('localStorage not accessible', '*');      
-    }
-  })();
+document.addEventListener('DOMContentLoaded', () => {  
+  try {
+    window.localStorage.setItem('test', 'test');    
+  } catch (error) {         
+    window.parent.postMessage('localStorage not accessible', '*');
+  }  
 });
